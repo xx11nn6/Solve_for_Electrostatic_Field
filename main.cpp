5f5903f62866c620773049c7aabdd686fceab924
xx11nn6 <994286948@qq.com>
2023-05-11 16:06:31 +0800

    第三次修改，添加了网格初始化中，对垂直方向h3,h4做初始化。下一步需要增加：对水平方向h1,h2初始化

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <malloc.h>
+#include <numeric>
 using namespace std;
 
 struct Grid_Array  //定义电场网格结构
@@ -8,6 +9,7 @@ struct Grid_Array  //定义电场网格结构
 	bool is_margin;  //判断网格点是否是边界，1表示是边界
 	bool on_axis;  //判断是否在轴上；
 	float h1, h2, h3, h4;  //存储网格点到周围的距离
+	float r;  //存储网格点的径向距离
 };
 
 
@@ -31,8 +33,8 @@ int main()
 	int _N[6];  //定义每个电极之间所取的网格数（水平方向），加下划线为了与全局变量N区别
 	int M1, M2;  //竖直方向网格数划分的要求
 	dz[0] = 5.2; dz[1] = 8.6; dz[2] = 8.6; dz[3] = 8.6; dz[4] = 8.6; dz[5] = 5.2;
-	_N[0] = 6; _N[1] = 10; _N[2] = 10; _N[3] = 10; _N[4] = 10; _N[5] = 4;
-	M1 = 23; M2 = 15;
+	_N[0] = 3; _N[1] = 5; _N[2] = 5; _N[3] = 5; _N[4] = 5; _N[5] = 2;
+	M1 = 11; M2 = 7;
 	//计算总网格数M与N
 	M = M1 + M2;
 	N = _N[0] + _N[1] + _N[2] + _N[3] + _N[4] + _N[5] + n - 1;
@@ -50,7 +52,7 @@ int main()
 	{
 		grid[i] = new Grid_Array[N];  //第二层指向列
 	}
-	grid_initialize(grid,V_c,V_s);
+	grid_initialize(grid,V_c,V_s,dz,_N,r1,r2,M1,M2,delta);
 	node_set(grid,3,1,50,true);
 
 
@@ -60,7 +62,7 @@ int main()
 	{
 		for (int j = 0; j < N; j++) 
 		{
-			cout << "(" << grid[i][j].voltage << ", " << grid[i][j].is_margin << ") ";
+			cout << "(" << grid[i][j].r << ", " << grid[i][j].h3 << "," << grid[i][j].h4 << ") ";
 		}
 		cout << endl;
 	}
@@ -79,7 +81,7 @@ void grid_initialize(Grid_Array** grid,float cathode_voltage,float screen_voltag
 					//输入电场网格，网格宽度、高度、阴极电压、荧光屏电压、网格间距（传入数组dz）、水平方向网格划分要求（传入数组_N）
 					//电极底到轴距r1、电极深度r2，垂直方向网格划分要求M1,M2、电极宽度delta
 {
-	//先将电位置零，全部设置为非边界、非轴上点
+	//先将电位置零，全部设置为非边界、非轴上点，以及竖直方向间距
 	for (int i = 0; i < M; i++)
 	{
 		for (int j = 0; j < N; j++)
@@ -87,6 +89,43 @@ void grid_initialize(Grid_Array** grid,float cathode_voltage,float screen_voltag
 			grid[i][j].voltage = 0;
 			grid[i][j].is_margin = false;
 			grid[i][j].on_axis = false;
+			//设置第一行
+			if (i == 0)
+			{
+				grid[i][j].h4 = 0;
+				grid[i][j].h3 = r2 / (M2 - 1);
+				grid[i][j].r = r1 + r2;
+			}
+			//设置1~M2-1行
+			else if (i < M2 - 1)
+			{
+				grid[i][j].h4 = r2 / (M2 - 1);
+				grid[i][j].h3 = grid[i][j].h4;
+				grid[i][j].r = r1 + r2 - ((r2 * i) / (M2 - 1));
+			}
+			//设置M2行
+			else if (i == M2 - 1)
+			{
+				grid[i][j].h4 = r2 / (M2 - 1);
+				grid[i][j].h3 = r1 / (M1 - 1);
+				grid[i][j].r = r1;
+			}
+			//设置M2~M1-1行
+			else if (i < (M2 + M1 - 1))
+			{
+				grid[i][j].h4 = r1 / (M1 - 1);
+				grid[i][j].h3 = grid[i][j].h4;
+				grid[i][j].r = r1 - ((r1 * (i - M2)) / (M1 - 1));
+			}
+			//设置轴上点
+			else
+			{
+				grid[i][j].h3 = 0;
+				grid[i][j].h4 = r1 / (M1 - 1);
+				grid[i][j].r = 0;
+				grid[i][j].on_axis = true;
+				grid[i][j].is_margin = true;
+			}
 		}
 	}
 	//设置光阴极
@@ -94,9 +133,10 @@ void grid_initialize(Grid_Array** grid,float cathode_voltage,float screen_voltag
 	{
 		grid[i][0].voltage = cathode_voltage;
 		grid[i][0].is_margin = true;
-		grid[i][0].h1 = 0;
-		grid[i][0].h2 = dz[0] / _N[0];
-		grid[i][0].h3=
+		grid[i][0].h1 = 0;  //光阴极为左边界，h1=0
+		grid[i][0].h2 = dz[0] / (_N[0] - 1);  //光阴极右边到第一个电极距离为dz[0],网格数为_N[0]，则可算出h2
+		
+		
 	}
 	//设置荧光屏电位和边界情况
 	for (int i = 0; i < M; i++)
