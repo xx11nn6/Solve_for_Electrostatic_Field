e2957faba470adca01670134dd38560dd9cfaa8c
xx11nn6 <994286948@qq.com>
2023-05-14 19:55:29 +0800

    第一类像管的边界封闭直接在初始化函数中完成 昨天头晕了，代码少些个电极= =

--- main.cpp
+++ main.cpp
@@ -18,27 +18,29 @@ struct Grid_Array  //定义电场网格结构
 int M, N;//网格数M*N
 int mode = 1;  //定义工作模式，1为第一类像管，2为第二类像管
 //声明函数
-void grid_initialize(Grid_Array** grid, double cathode_voltage, double screen_voltage,int n, double* dz, int* _N, double r1, double r2, int M1, int M2, double delta);
-void node_set(Grid_Array** grid, int m, int n, double voltage, bool is_margin);
+void grid_initialize_mode_1(Grid_Array** grid, double cathode_voltage, double screen_voltage,int n, double* dz, int* _N, double* V, double r1, double r2, int M1, int M2, double delta);
 
 
 int main()
 {
 	//初始化
-	double V_c = 0, V_s = 100;  //定义光阴极和荧光屏电压分别为0和100V
-	int n = 7;  //定义电极数（包含荧光屏，不包含阴极）
-	double delta = 0.5;//定义电极宽度δ
-	double z0 = 53.4, r0 = 32;  //定义径向和轴向的宽度
-	double r2 = 12, r1 = r0 - r2;  //r2为电极插入电场的深度，r1为电极底端到轴的距离
-	double dz[6];  //定义每个电极的间距
-	int _N[6];  //定义每个电极之间所取的网格数（水平方向），加下划线为了与全局变量N区别
-	int M1, M2;  //竖直方向网格数划分的要求
-	dz[0] = 5.2; dz[1] = 8.6; dz[2] = 8.6; dz[3] = 8.6; dz[4] = 8.6; dz[5] = 5.2;
-	_N[0] = 3; _N[1] = 5; _N[2] = 5; _N[3] = 5; _N[4] = 5; _N[5] = 2;
+	double V_c = 0, V_s = 100;		//定义光阴极和荧光屏电压分别为0和100V
+	int n = 7;						//定义电极数（包含荧光屏，不包含阴极）
+	double delta = 0.5;				//定义电极宽度δ
+	double z0 = 53.4, r0 = 32;		//定义径向和轴向的宽度
+	double r2 = 12, r1 = r0 - r2;   //r2为电极插入电场的深度，r1为电极底端到轴的距离
+	double dz[7];				    //定义每个电极的间距
+	int _N[7];						//定义每个电极之间所取的网格数（水平方向），加下划线为了与全局变量N区别
+	double V[6];					//定义各个电极电压
+	int M1, M2;					    //竖直方向网格数划分的要求
+
+	V[0] = 24; V[1] = 40; V[2] = 62; V[3] = 74; V[4] = 85; V[5] = 96;
+	dz[0] = 5.2; dz[1] = 8.6; dz[2] = 8.6; dz[3] = 8.6; dz[4] = 8.6; dz[5] = 8.6; dz[6] = 5.2;
+	_N[0] = 3; _N[1] = 5; _N[2] = 5; _N[3] = 5; _N[4] = 5; _N[5] = 5; _N[6] = 2;
 	M1 = 11; M2 = 7;
 	//计算总网格数M与N
 	M = M1 + M2;
-	N = _N[0] + _N[1] + _N[2] + _N[3] + _N[4] + _N[5] + 2 * (n - 1); //n个电极有n-1个区间，因此乘以n-1
+	N = _N[0] + _N[1] + _N[2] + _N[3] + _N[4] + _N[5] +_N[6]+ 2 * n; 
 	double epsilon = 0.0005;  //迭代精度为0.005
 
 
@@ -53,8 +55,7 @@ int main()
 	{
 		grid[i] = new Grid_Array[N];  //第二层指向列
 	}
-	grid_initialize(grid,V_c,V_s,n,dz,_N,r1,r2,M1,M2,delta);
-	node_set(grid,3,1,50,false);
+	grid_initialize_mode_1(grid,V_c,V_s,n,dz,_N,V,r1,r2,M1,M2,delta);
 
 
 	//c++中用cout来输出，使用cout输出矩阵
@@ -63,8 +64,8 @@ int main()
 	{
 		for (int j = 0; j < N; j++) 
 		{
-			//cout << " " << grid[i][j].on_axis << " ";
-			cout << "(" << setprecision(2) << grid[i][j].h1 << "," << setprecision(2) << grid[i][j].h2 << ") ";
+			cout << " " << setfill(' ') << setw(4) << setprecision(3) << grid[i][j].voltage << " ";  //setfill等函数是iomanip库中的函数，用于控制输出格式
+			//cout << "(" << setprecision(2) << grid[i][j].h1 << "," << setprecision(2) << grid[i][j].h2 << ") ";
 		}
 		cout << endl;
 	}
@@ -77,13 +78,11 @@ int main()
 
 
 
-//用于初始化电场网格
-void grid_initialize(Grid_Array** grid,double cathode_voltage,double screen_voltage,int n,double* dz,int* _N,double r1,double r2,int M1,int M2,double delta)
-					//输入电场网格，网格宽度、高度、阴极电压、荧光屏电压、电极个数n、网格间距（传入数组dz）、水平方向网格划分要求（传入数组_N）
+//用于初始化第一类像管的电场网格
+void grid_initialize_mode_1(Grid_Array** grid,double cathode_voltage,double screen_voltage,int n,double* dz,int* _N,double* V,double r1,double r2,int M1,int M2,double delta)
+					//输入电场网格，网格宽度、高度、阴极电压、荧光屏电压、电极个数n、网格间距（传入数组dz）、水平方向网格划分要求（传入数组_N）、电极电压（数组V）
 					//电极底到轴距r1、电极深度r2，垂直方向网格划分要求M1,M2、电极宽度delta
 {
-	//电极区间数n=n-1
-	n = n - 1;
 	//遍历网格进行初始化
 	for (int i = 0; i < M; i++)
 	{
@@ -149,6 +148,22 @@ void grid_initialize(Grid_Array** grid,double cathode_voltage,double screen_volt
 				{
 					if (j <= sum)
 					{
+						if (i == 0)  //如果i=0直接做封闭边界处理
+						{
+							grid[i][j].is_margin = true;
+							if (k == 0)  //若在阴极和第一个电极间，线性插值计算边界电压
+							{
+								grid[i][j].voltage = (V[0] - cathode_voltage) * j / (_N[0] + 1);
+							}
+							else if (k != (n - 1))  //若不在最后一个电极到荧光屏区间
+							{
+								grid[i][j].voltage = V[k - 1] + (V[k] - V[k - 1]) * (_N[k] + j - sum) / (_N[k] + 1);
+							}
+							else  //最后一个电极到荧光屏区间
+							{
+								grid[i][j].voltage = V[k - 1] + (screen_voltage - V[k - 1]) * (_N[k] + j - sum) / (_N[k] + 1);
+							}
+						}
 						grid[i][j].h1 = dz[k] / (_N[k]);
 						grid[i][j].h2 = dz[k] / _N[k];
 						break;
@@ -158,6 +173,7 @@ void grid_initialize(Grid_Array** grid,double cathode_voltage,double screen_volt
 						if (i < M2)  //若高度在电极深度范围内
 						{
 							grid[i][j].is_margin = true;
+							grid[i][j].voltage = V[k];
 						}
 						grid[i][j].h1 = dz[k] / _N[k];
 						grid[i][j].h2 = delta;
@@ -168,13 +184,14 @@ void grid_initialize(Grid_Array** grid,double cathode_voltage,double screen_volt
 						if (i < M2)  //若高度在电极深度范围内
 						{
 							grid[i][j].is_margin = true;
+							grid[i][j].voltage = V[k];
 						}
 						grid[i][j].h1 = delta;
 						grid[i][j].h2 = dz[k + 1] / _N[k + 1];
 						break;
 					}
 
-					else if (k == (n - 1))  //最后一组
+					else if (k == (n-1) ) //最后一组
 					{
 						if (j <= (sum + 1)) //在荧光屏上
 						{
@@ -204,18 +221,7 @@ void grid_initialize(Grid_Array** grid,double cathode_voltage,double screen_volt
 	}
 }
 
-//用于给指定坐标的网格节点设定电压值和边界状态，输入grid数组，坐标（m,n），电压，是否是边界
-void node_set(Grid_Array** grid,int m,int n,double voltage,bool is_margin)
-{
-	grid[m][n].voltage = voltage;
-	grid[m][n].is_margin = is_margin;
-}
 
-//边界封闭函数，用于使静电场
-void boundary_close()
-{
-
-}
 
 //c++中使用delete来释放内存
 void free(Grid_Array** grid)
