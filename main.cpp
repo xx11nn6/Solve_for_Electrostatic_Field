92c5127a684fd958d756d8d0281f17cd7b1b28c4
xx11nn6 <994286948@qq.com>
2023-05-16 17:54:22 +0800

    按设计要求重新修改网格划分 （M1、M2，N1…N7是格数而不是网数……）

--- main.cpp
+++ main.cpp
@@ -24,23 +24,24 @@ void grid_initialize_mode_1(Grid_Array** grid, double cathode_voltage, double sc
 int main()
 {
 	//初始化
+	//////////////////注意！！M*N为网数，而_N与M1、M2是格数///////////////////////////////
 	double V_c = 0, V_s = 100;		//定义光阴极和荧光屏电压分别为0和100V
 	int n = 7;						//定义电极数（包含荧光屏，不包含阴极）
 	double delta = 0.5;				//定义电极宽度δ
 	double z0 = 53.4, r0 = 32;		//定义径向和轴向的宽度
 	double r2 = 12, r1 = r0 - r2;   //r2为电极插入电场的深度，r1为电极底端到轴的距离
 	double dz[7];				    //定义每个电极的间距
-	int _N[7];						//定义每个电极之间所取的网格数（水平方向），加下划线为了与全局变量N区别
+	int _N[7];						//定义每个电极之间所取的格数（水平方向），加下划线为了与全局变量N区别
 	double V[6];					//定义各个电极电压
-	int M1, M2;					    //竖直方向网格数划分的要求
+	int M1, M2;					    //竖直方向格数划分的要求
 
 	V[0] = 24; V[1] = 40; V[2] = 62; V[3] = 74; V[4] = 85; V[5] = 96;
 	dz[0] = 5.2; dz[1] = 8.6; dz[2] = 8.6; dz[3] = 8.6; dz[4] = 8.6; dz[5] = 8.6; dz[6] = 5.2;
 	_N[0] = 3; _N[1] = 5; _N[2] = 5; _N[3] = 5; _N[4] = 5; _N[5] = 5; _N[6] = 2;
 	M1 = 11; M2 = 7;
-	//计算总网格数M与N
-	M = M1 + M2;
-	N = _N[0] + _N[1] + _N[2] + _N[3] + _N[4] + _N[5] +_N[6]+ 2 * n; 
+	//计算总网数M与N
+	M = M1 + M2 + 1;  //格数是M1+M2,网数还要加一
+	N = _N[0] + _N[1] + _N[2] + _N[3] + _N[4] + _N[5] +_N[6]+ n; //有_N+n-1个格数，网数需要加一
 	double epsilon = 0.0005;  //迭代精度为0.005
 
 
@@ -65,7 +66,7 @@ int main()
 		for (int j = 0; j < N; j++) 
 		{
 			cout << " " << setfill(' ') << setw(4) << setprecision(3) << grid[i][j].voltage << " ";  //setfill等函数是iomanip库中的函数，用于控制输出格式
-			//cout << "(" << setprecision(2) << grid[i][j].h1 << "," << setprecision(2) << grid[i][j].h2 << ") ";
+			//cout << "(" << setprecision(2) << grid[i][j].h3 << "," << setprecision(2) << grid[i][j].h4 << ") ";
 		}
 		cout << endl;
 	}
@@ -97,35 +98,35 @@ void grid_initialize_mode_1(Grid_Array** grid,double cathode_voltage,double scre
 			if (i == 0)
 			{
 				grid[i][j].h4 = 0;
-				grid[i][j].h3 = r2 / (M2 - 1);
+				grid[i][j].h3 = r2 / M2;
 				grid[i][j].r = r1 + r2;
 			}
-			//设置1~M2-1行
-			else if (i < (M2 - 1))
+			//设置1~M2行
+			else if (i < M2)
 			{
-				grid[i][j].h4 = r2 / (M2 - 1);
+				grid[i][j].h4 = r2 / M2;
 				grid[i][j].h3 = grid[i][j].h4;
-				grid[i][j].r = r1 + r2 - ((r2 * i) / (M2 - 1));
+				grid[i][j].r = r1 + r2 - ((r2 * i) / M2);
 			}
 			//设置M2行
-			else if (i == (M2 - 1))
+			else if (i == M2)
 			{
-				grid[i][j].h4 = r2 / (M2 - 1);
-				grid[i][j].h3 = r1 / (M1 - 1);
+				grid[i][j].h4 = r2 / M2;
+				grid[i][j].h3 = r1 / M1;
 				grid[i][j].r = r1;
 			}
-			//设置M2~M1-1行
-			else if (i < (M2 + M1 - 1))
+			//设置M2~M1行
+			else if (i < (M2 + M1))
 			{
-				grid[i][j].h4 = r1 / (M1 - 1);
+				grid[i][j].h4 = r1 / M1;
 				grid[i][j].h3 = grid[i][j].h4;
-				grid[i][j].r = r1 - ((r1 * (i - M2)) / (M1 - 1));
+				grid[i][j].r = r1 - ((r1 * (i - M2)) / M1);
 			}
 			//设置轴上点
 			else
 			{
 				grid[i][j].h3 = 0;
-				grid[i][j].h4 = r1 / (M1 - 1);
+				grid[i][j].h4 = r1 / M1;
 				grid[i][j].r = 0;
 				grid[i][j].on_axis = true;
 				grid[i][j].is_margin = true;
@@ -146,31 +147,31 @@ void grid_initialize_mode_1(Grid_Array** grid,double cathode_voltage,double scre
 				int k = 0;
 				while(k < n)
 				{
-					if (j <= sum)
+					if (j < sum)
 					{
-						if (i == 0)  //如果i=0直接做封闭边界处理
+						if (i == 0)  //如果i=0（第一行）直接做封闭边界处理
 						{
 							grid[i][j].is_margin = true;
 							if (k == 0)  //若在阴极和第一个电极间，线性插值计算边界电压
 							{
-								grid[i][j].voltage = (V[0] - cathode_voltage) * j / (_N[0] + 1);
+								grid[i][j].voltage = (V[0] - cathode_voltage) * j / _N[0];
 							}
 							else if (k != (n - 1))  //若不在最后一个电极到荧光屏区间
 							{
-								grid[i][j].voltage = V[k - 1] + (V[k] - V[k - 1]) * (_N[k] + j - sum) / (_N[k] + 1);
+								grid[i][j].voltage = V[k - 1] + (V[k] - V[k - 1]) * (_N[k] + j - sum) / _N[k];
 							}
 							else  //最后一个电极到荧光屏区间
 							{
-								grid[i][j].voltage = V[k - 1] + (screen_voltage - V[k - 1]) * (_N[k] + j - sum) / (_N[k] + 1);
+								grid[i][j].voltage = V[k - 1] + (screen_voltage - V[k - 1]) * (_N[k] + j - sum) / _N[k];
 							}
 						}
-						grid[i][j].h1 = dz[k] / (_N[k]);
+						grid[i][j].h1 = dz[k] / _N[k];
 						grid[i][j].h2 = dz[k] / _N[k];
 						break;
 					}
-					else if (j <= (sum + 1) && (k != (n - 1))) //在电极左侧且不在荧光屏
+					else if (j == sum && (k != (n - 1))) //在电极左侧且不在荧光屏
 					{
-						if (i < M2)  //若高度在电极深度范围内
+						if (i <= M2)  //若高度在电极深度范围内
 						{
 							grid[i][j].is_margin = true;
 							grid[i][j].voltage = V[k];
@@ -179,9 +180,9 @@ void grid_initialize_mode_1(Grid_Array** grid,double cathode_voltage,double scre
 						grid[i][j].h2 = delta;
 						break;
 					}
-					else if (j <= (sum + 2) && (k != (n - 1)))  //在电极右侧且不在荧光屏
+					else if (j == (sum + 1) && (k != (n - 1)))  //在电极右侧且不在荧光屏
 					{
-						if (i < M2)  //若高度在电极深度范围内
+						if (i <= M2)  //若高度在电极深度范围内
 						{
 							grid[i][j].is_margin = true;
 							grid[i][j].voltage = V[k];
@@ -193,7 +194,7 @@ void grid_initialize_mode_1(Grid_Array** grid,double cathode_voltage,double scre
 
 					else if (k == (n-1) ) //最后一组
 					{
-						if (j <= (sum + 1)) //在荧光屏上
+						if (j == sum ) //在荧光屏上
 						{
 							grid[i][j].h1 = dz[k] / _N[k];
 							grid[i][j].h2 = 0;
@@ -204,7 +205,7 @@ void grid_initialize_mode_1(Grid_Array** grid,double cathode_voltage,double scre
 					else  //若越过第一个电极
 					{
 						k += 1;
-						sum = sum + _N[k] + 2;
+						sum = sum + _N[k] + 1;
 						continue;
 					}
 				}
